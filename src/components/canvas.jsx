0585d1434392c831d5acaf190d84872563640577
swhite21 <sebastian.swhite@gmail.com>
2021-03-26 02:09:27 -0700

    added default cube

---
+++ src/components/canvas.jsx
@@ -0,0 +1,22 @@
+import { Canvas } from "react-three-fiber";
+import styled from "styled-components";
+import BoxMesh from "./threeComponents/boxMesh";
+
+const ThreeCanvas = () => {
+  return (
+    <StyledCanvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <BoxMesh position={[0, 0, 0]} />
+    </StyledCanvas>
+  );
+};
+
+export default ThreeCanvas;
+
+const StyledCanvas = styled(Canvas)`
+  background-color: black;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+`;
---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+const Header = () => {
+  return (
+    <Container>
+      <Title>React Three Fiber Starter</Title>
+    </Container>
+  );
+};
+
+export default Header;
+
+const Container = styled.div`
+  background-color: #000e35;
+  display: flex;
+  flex: 1;
+  padding: 0.8rem;
+`;
+const Title = styled.h1`
+  color: white;
+  font-size: 1.3rem;
+  padding: 0;
+  margin: 0;
+`;
